1e7d3cae3d6c9bcffa47e624150d6b8864542918
Bruno Bonamin <bruno@bonamin.org>
2013-02-18 12:41:27 -0300

    Displays new html object form at the top of the document

--- selector.js
+++ selector.js
@@ -1,39 +1,44 @@
 $(document).ready(function(){
-	
-	$('body').on('mouseover mouseout click','*', function(event) {
-		switch (event.type){
-			case 'mouseover':
-				$(this).data('bgcolor', $(this).css('background-color'));
-	        	$(this).css('background-color','rgba(255,0,0,.5)');
-				break;
-			case 'mouseout':
-				$(this).css('background-color', $(this).data('bgcolor'));
-				break;
-			case 'click':
-					console.log($(this));
-					xpath = Selector.xpath(event);
-					chrome.extension.sendMessage({"xpath": xpath}, function(response) {});
-				break;
-		}
-	    return false;
-	});
+  $('body').prepend('<div id="topbar" style="display:none"></div>');
+  $('#topbar').load(chrome.extension.getURL('new_object_map/form.html'), function(){
+    $(this).slideDown();
+  });
+
+
+  $('body').on('mouseover mouseout click','*', function(event) {
+    switch (event.type){
+      case 'mouseover':
+        $(this).data('bgcolor', $(this).css('background-color'));
+            $(this).css('background-color','rgba(255,0,0,.5)');
+        break;
+      case 'mouseout':
+        $(this).css('background-color', $(this).data('bgcolor'));
+        break;
+      case 'click':
+          console.log($(this));
+          xpath = Selector.xpath(event);
+          chrome.extension.sendMessage({"xpath": xpath}, function(response) {});
+        break;
+    }
+      return false;
+  });
 
 
 });
 
 Selector = {
-	xpath: function(event){
-	    var hierarchy = [],
-	        current = event.toElement||event.target;
+  xpath: function(event){
+      var hierarchy = [],
+          current = event.toElement||event.target;
 
-	    while (current.parentNode){
-	        hierarchy.unshift(current);
-	        current = current.parentNode;
-	    }
-	    var xPath = hierarchy.map(function(el,i){
-	            return el.nodeName.toLowerCase() + ((el.id !== '') ? '#'+el.id : '') + ((el.className !== '') ? '.'+el.className.split(' ').join('.') : '');
-	        }).join('/');
-	
-	    return xPath;
-	}
+      while (current.parentNode){
+          hierarchy.unshift(current);
+          current = current.parentNode;
+      }
+      var xPath = hierarchy.map(function(el,i){
+              return el.nodeName.toLowerCase() + ((el.id !== '') ? '#'+el.id : '') + ((el.className !== '') ? '.'+el.className.split(' ').join('.') : '');
+          }).join('/');
+  
+      return xPath;
+  }
 }
