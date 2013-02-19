876887ef8a900a460343ad654e8ef19f1844940f
seba <seba.as@gmail.com>
2013-02-19 11:33:18 -0300

    Object Type

--- selector.js
+++ selector.js
@@ -1,6 +1,14 @@
 Selector = {
 	_:{
-		already_opened: false
+		type_hash: {"A":"link",
+			"BUTTON": "button", 
+			"DIV": "text", 
+			"INPUT": 
+				{"text": "text_box", 
+				 "chackbox": "checkbox", 
+				 "password":"password", 
+				 "radio": "radio_button"}, 
+			"SPAN": "text"}
 	},
 	xpath: function(event){
 	    var hierarchy = [],
@@ -15,6 +23,34 @@ Selector = {
 	        }).join('/');
 	
 	    return xPath;
+	},
+	suggested_name: function(elem_name,elem_content){
+		if(elem_name != undefined && elem_name != "") {
+			return elem_name;
+		} else {
+			var regex = new RegExp(" ", "g");
+			var sug_name = elem_content.replace(regex,"_").substring(0,32);
+			return sug_name;
+		}
+	},
+	object_locator: function(elem_id,elem_xpath){
+
+		if(elem_id != undefined && elem_id != "") {
+			return "id : " + elem_id
+		} else {	
+	        return "xpath : " + elem_xpath;
+		}
+	},
+	object_type: function(tag,type){
+		var tag_type = Selector._.type_hash[tag];
+		if(tag_type == undefined) {
+			return "unknown";
+		}
+		if(typeof(tag_type) == "string") { 
+			return tag_type;
+		} else {
+			return tag_type[type];
+		}
 	},
 	highlighter: function(event){
 		switch (event.type){
@@ -38,37 +74,41 @@ Selector = {
 					event.stopPropagation();
 					event.preventDefault();
 
-          $('input[name*="new_object_"]').val();
+					$('input[name*="new_object_"]').val();
 
 					element_xpath = Selector.xpath(event);
-          $('input[name="new_object_xpath"]').val(element_xpath);
+					$('input[name="new_object_xpath"]').val(element_xpath);
 
-          element_id = this.id;
-          $('input[name="new_object_id"]').val(element_id);
+					element_id = this.id;
+					$('input[name="new_object_id"]').val(element_id);
 
 					element_class = this.classList.toString();
-          $('input[name="new_object_class"]').val(element_class);
+					$('input[name="new_object_class"]').val(element_class);
 
 					element_tag = this.tagName;
-          $('input[name="new_object_tag"]').val(element_tag);
+					$('input[name="new_object_tag"]').val(element_tag);
 
 					element_name = this.name;
-          $('input[name="new_object_name"]').val(element_name);
+					$('input[name="new_object_name"]').val(element_name);
 
 					element_type = this.type;
-          $('input[name="new_object_type"]').val(element_type);
+					$('input[name="new_object_type"]').val(element_type);
 
 					element_content = $(this).html();
-          $('input[name="new_object_content"]').val(element_content);
+					$('input[name="new_object_content"]').val(element_content);
 
-					console.log("this", this);
-					console.log("xpath", element_xpath);
-					console.log("id", element_id);
-					console.log("class", element_class);
-					console.log("tag", element_tag);
-					console.log("name", element_name);
-					console.log("type", element_type);
-					console.log("content", element_content);
+
+					$('#suggested_name').html(Selector.suggested_name(element_name,element_content));
+					$('#object_locator').html(Selector.object_locator(element_id,element_xpath));
+					$('#object_type').html(Selector.object_type(element_tag,element_type));
+					// console.log("this", this);
+					// console.log("xpath", element_xpath);
+					// console.log("id", element_id);
+					// console.log("class", element_class);
+					// console.log("tag", element_tag);
+					// console.log("name", element_name);
+					// console.log("type", element_type);
+					// console.log("content", element_content);
 					chrome.extension.sendMessage({"xpath": element_xpath}, function(response) {});
 					$('body').off('mouseover mouseout click','*');
 
@@ -80,8 +120,7 @@ Selector = {
 }
 
 $(document).ready(function(){
-	if(!Selector._.already_opened) {
-		Selector._.already_opened = true;
+
     $('body').prepend('<div id="topbar" style="display:none"></div>');
     $('#topbar').load(chrome.extension.getURL('new_object_map/form.html'), function(){
       $(this).slideDown(function(){
@@ -92,7 +131,7 @@ $(document).ready(function(){
       });
     });
 	  $('body > :not(#topbar)').on('mouseover mouseout click','*', Selector.highlighter);
-  }
+
 
 
 
